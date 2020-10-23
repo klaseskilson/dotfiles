daa8c35b0e9984152b6bbe0e9dc2cc980264a3f2
Klas Eskilson <klas.eskilson@gmail.com>
2020-10-23 14:53:25 +0200

    fix symlinking in setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ brew tap Homebrew/bundle
 # Install tools and apps
 ./Brewfile.sh
 curl https://sh.rustup.rs -sSf | sh
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # initialize rbenv
 rbenv init
@@ -26,11 +27,12 @@ killall Dock
 mkdir ~/.config/nvim
 
 # Create symlinks for dotfiles
-ln -s ~/Development/dotfiles/.zshrc ~/.zshrc
-ln -s ~/Development/dotfiles/.vimrc ~/.config/nvim/init.vim
-ln -s ~/Development/dotfiles/.gitconfig ~/.gitconfig
-ln -s ~/Development/dotfiles/.gitignore_global ~/.gitignore_global
-ln -s ~/Development/dotfiles/sublime/Package\ Control.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
-ln -s ~/Development/dotfiles/sublime/Default\ \(OSX\).sublime-keymap ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
-ln -s ~/Development/dotfiles/sublime/Preferences.sublime-settings ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-ln -s ~/Development/dotfiles/.agignore ~/.agignore
+ln -s "$(pwd)/.zshrc" ~/.zshrc
+ln -s "$(pwd)/.tmux.conf" ~/.tmux.conf
+ln -s "$(pwd)/.vimrc" ~/.config/nvim/init.vim
+ln -s "$(pwd)/.gitconfig" ~/.gitconfig
+ln -s "$(pwd)/.gitignore_global" ~/.gitignore_global
+ln -s "$(pwd)/sublime/Package\ Control.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
+ln -s "$(pwd)/sublime/Default\ \(OSX\).sublime-keymap" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Default\ \(OSX\).sublime-keymap
+ln -s "$(pwd)/sublime/Preferences.sublime-settings" ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+ln -s "$(pwd)/.agignore" ~/.agignore
